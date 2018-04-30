5b268f03552238faa4e38939b916f7611681677a
Austin Loveless <austinloveless@github.com>
2018-04-30 11:51:43 -0600

    added api

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -1,19 +1,18 @@
-import React from 'react';
-
+import React from "react";
 
 class AddItem extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      name: '',
+      name: "",
       priceInCents: 0,
       quantity: 1
-    }
+    };
   }
 
-onSubmit = (e) => {
-  e.preventDefault()
-    const { id, name, priceInCents, quantity } = this.state
+  onSubmit = e => {
+    e.preventDefault();
+    const { id, name, priceInCents, quantity } = this.state;
 
     const newProduct = {
       product: {
@@ -22,58 +21,61 @@ onSubmit = (e) => {
         priceInCents
       },
       quantity
-    }
-    console.log('log', newProduct)
-  this.props.addItems(newProduct)
+    };
+    console.log("log", newProduct);
+    this.props.addItems(newProduct);
+  };
+
+  onSelectChange = (e) => {
+    const id = e.target.value;
+    const { products } = this.props;
+    const product = products.filter(element => {
+      return +id === +element.id;
+    });
+
+    this.setState({
+      name: product[0].name,
+      priceInCents: product[0].priceInCents
+    });
+  };
+
+  renderProductOption = () => {
+    const { products } = this.props;
+    return products.map(product => (
+      <option  key={product.id} value={product.id}>
+        {product.name}
+      </option>
+    ));
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.onSubmit}>
+        <p>
+          <label>
+            Quantity:
+            <input
+              type="text"
+              onChange={e => this.setState({ quantity: e.target.value })}
+              name="quantity"
+            />
+          </label>
+        </p>
+        <p>
+          <label>
+            Items:
+            <select  onChange={this.onSelectChange}>
+              <option>Select an item... </option>
+              {this.renderProductOption()}
+            </select>
+          </label>
+        </p>
+        <p>
+          <button type="submit">Submit</button>
+        </p>
+      </form>
+    );
+  }
 }
 
-onSelectChange = (e) => {
-  const index = e.nativeEvent.target.selectedIndex
-  const select = e.nativeEvent.target
-  const name = select[index].text
-  const priceInCents = e.target.value
-
-  this.setState({ name, priceInCents })
-}
-
-renderProductOption = () => {
-  const { products } = this.props
-  return products.map((product) => (
-    <option key={product.id} value={product.priceInCents}>
-      {product.name}
-    </option>
-  ))
-}
-
-render() {
-  return(
-  <form onSubmit={this.onSubmit}>
-    <p>
-      <label>
-      Quantity:
-       <input
-       type ="text"
-       onChange={ (e) => this.setState({quantity: e.target.value}) }
-       name="quantity"/>
-      </label>
-    </p>
-    <p>
-      <label> Items:
-        <select onChange={this.onSelectChange}>
-          <option>Select an item... </option>
-          { this.renderProductOption() }
-        </select>
-      </label>
-    </p>
-    <p>
-    <button type="submit">Submit</button>
-    </p>
-  </form>
-  )
-}
-
-}
-
-
-
-export default AddItem
+export default AddItem;
--- src/components/CartItems.js
+++ src/components/CartItems.js
@@ -1,39 +1,37 @@
-import React from 'react';
+import React from "react";
 
-const CartItems = (props) => {
+const CartItems = props => {
   let listItems = props.items.map((items, i) => {
-      return (
-        <div key={i}>
-          <div className="list-group">
-            <div className="list-group-item">
-              <div className="row">
-          <div className="col-md-8">{ items.product.name }</div>
-          <div className="col-md-2">{ items.product.priceInCents }</div>
-          <div className="col-md-2">{ items.quantity }</div>
-        </div>
-          </div>
+    return (
+      <div key={i}>
+        <div className="list-group">
+          <div className="list-group-item">
+            <div className="row">
+              <div className="col-md-8">{items.product.name}</div>
+              <div className="col-md-2">{items.product.priceInCents}</div>
+              <div className="col-md-2">{items.quantity}</div>
             </div>
-              </div>
-
-
-      )
-  })
+          </div>
+        </div>
+      </div>
+    );
+  });
 
   return (
     <div className="container">
-  <h1>Cart Items</h1>
-  <div className="list-group">
-    <div className="list-group-item">
-      <div className="row">
-        <div className="col-md-8">Product</div>
-        <div className="col-md-2">Price</div>
-        <div className="col-md-2">Quantity</div>
+      <h1>Cart Items</h1>
+      <div className="list-group">
+        <div className="list-group-item">
+          <div className="row">
+            <div className="col-md-8">Product</div>
+            <div className="col-md-2">Price</div>
+            <div className="col-md-2">Quantity</div>
+          </div>
+        </div>
+        {listItems}
       </div>
     </div>
-    {listItems}
-  </div>
-</div>
-  )
-}
+  );
+};
 
-export default CartItems
+export default CartItems;
